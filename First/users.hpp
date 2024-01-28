ff15c08adf1a0480468375b340a3c6d8660ac03e
JustUsingaWebsite <2022156892@ub.edu.bz>
2024-01-27 18:15:37 -0600

    oop addition

---
+++ First/users.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** users
+** File description:
+** user class
+*/
+
+#ifndef USERS_HPP_
+#define USERS_HPP_
+#include <iostream>
+using namespace std;
+
+class User{
+    protected:
+    int userId;
+    string pass;
+
+    public:
+    tuple<int, string> getuser(int);
+    User* adduser(int);
+    User* removeuser(int);
+
+};
+
+class Customer: public User{
+    private:
+    string fname;
+    string lname;
+    int age;
+
+    public:
+    void update_name(string = "", string = "");
+};
+
+#endif /* !USERS_HPP_ */
